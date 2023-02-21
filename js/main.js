67f6b8e391a7ac5459ee0953656c45e4caadcf71
sophia0307 <sonorous0307@gmail.com>
2023-02-22 03:53:30 +0900

    Update : header > top_nav
    header > top_nav : Markup, Style 진행 중

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 'use strict'; // 엄격모드on
+
+// clock - 한국의 현재 날짜와 시간
+const date = new Date();
+const today = new Intl.DateTimeFormat('en',{dateStyle:"full",timeStyle:"short"}).format(date);
+const dateContent = document.getElementById('date');
+
+dateContent.innerText = `${today}`;
+
+
